43a3b2ed40cdba08386932d9da2831bd6a4866ef
tavo0earth <tavo0earth@gmail.com>
2019-09-26 00:19:25 +0300

    Computer and player hits created

--- game.js
+++ game.js
@@ -24,16 +24,7 @@ class Game {
 
     stepSelection = () => {
         const step = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-
-        if (step === 1) {
-            return this.stepOne() + step
-        }
-        if (step === 2) {
-            return this.stepTwo()
-        }
-        if (step === 3) {
-            return this.stepTree()
-        }
+            return step
     };
 
     game = () => {
@@ -42,19 +33,50 @@ class Game {
 
         while (healthComputer > 0 && healthPlayer > 0) {
             let stepComputer = this.stepSelection();
-            console.log('Удар по компьютру:' + stepComputer);
-
-            healthComputer = healthComputer - stepComputer;
-            console.log('Здоровье компьютера:' + healthComputer);
+            console.log('Шаг компьютра:' + stepComputer);
+            if (stepComputer === 1) {
+                let hitOnComputer = this.stepOne();
+                console.log('Будет нанесен удар:' + hitOnComputer);
+                healthComputer = healthComputer - hitOnComputer;
+                console.log('Здоровье компьютера:' + healthComputer);
+            }
+            if (stepComputer === 2) {
+                let hitOnComputer = this.stepTwo();
+                console.log('Будет нанесен удар:' + hitOnComputer);
+                healthComputer = healthComputer - hitOnComputer;
+                console.log('Здоровье компьютера:' + healthComputer);
+            }
+            if (stepComputer === 3) {
+                let hitOnComputer = this.stepTree();
+                console.log('Будет исцелен:' + hitOnComputer);
+                healthComputer = healthComputer + hitOnComputer;
+                console.log('Здоровье компьютера:' + healthComputer);
+            }
             if (healthComputer <= 0) {
                 console.log('Убит компьютер');
                 break;
             }
 
             let stepPlayer = this.stepSelection();
-            console.log('Удар по игроку:' + stepPlayer);
-            healthPlayer = healthPlayer - stepPlayer;
-            console.log('Здоровье игрока:' + healthPlayer);
+            console.log('Шаг игрока:' + stepPlayer);
+            if (stepPlayer === 1) {
+                let hitOnPlayer = this.stepOne();
+                console.log('Будет нанесен удар:' + hitOnPlayer);
+                healthPlayer = healthPlayer - hitOnPlayer;
+                console.log('Здоровье игрока:' + healthPlayer);
+            }
+            if (stepPlayer === 2) {
+                let hitOnPlayer = this.stepTwo();
+                console.log('Будет нанесен удар:' + hitOnPlayer);
+                healthPlayer = healthPlayer - hitOnPlayer;
+                console.log('Здоровье игрока:' + healthPlayer);
+            }
+            if (stepPlayer === 3) {
+                let hitOnPlayer = this.stepTree();
+                console.log('Будет исцелен:' + hitOnPlayer);
+                healthPlayer = healthPlayer + hitOnPlayer;
+                console.log('Здоровье игрока:' + healthPlayer);
+            }
             if (healthPlayer <= 0) {
                 console.log('Убит игрок');
             }
